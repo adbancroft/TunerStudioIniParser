1fb5a8783fd1b355acf37170b101f3ec9aa0ebe7
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2021-08-23 08:11:15 -0500

    Drive the transformer via the grammar

--- TsIniParser/TsParseTreeTransformer.py
+++ TsIniParser/TsParseTreeTransformer.py
@@ -9,7 +9,7 @@ from .TsIniFile import *
 class TsParseTreeTransformer(TransformerChain):
 
     def __init__(self):
-        super().__init__(_TsIniTransformerPhase1() * _TsIniTransformerPhase2())
+        super().__init__(_TsIniTransformerPhase1())# * _TsIniTransformerPhase2())
 
 class _TsIniTransformerPhase2(Transformer):
 
@@ -78,94 +78,132 @@ class _TsIniTransformerPhase2(Transformer):
         children = [c for c in children if c[0]!='line_label'] + [line_labels]
         return Curve(**dict(children))
 
-    def axis_limits(self, children):
-        return Axis(**dict(children))
+    # def axis_limits(self, children):
+    #     return Axis(**dict(children))
 
-    def axis_bin(self, children):
-        return AxisBin(**dict(children))
+    # def axis_bin(self, children):
+    #     return AxisBin(**dict(children))
 
-    # Collapse these to tuples so we can use them in dictionary comprehensions
-    @v_args(tree=True)
-    def _to_tuple(self, tree):
-        return (tree.data, tree.children[0] if len(tree.children)==1 else tree.children)
+    # # Collapse these to tuples so we can use them in dictionary comprehensions
+    # @v_args(tree=True)
+    # def _to_tuple(self, tree):
+    #     return (tree.data, tree.children[0] if len(tree.children)==1 else tree.children)
 
-    xaxis_limits = _to_tuple
-    yaxis_limits = _to_tuple
-    xbins = _to_tuple
-    ybins = _to_tuple
-    zbins = _to_tuple
+    # xaxis_limits = _to_tuple
+    # yaxis_limits = _to_tuple
+    # xbins = _to_tuple
+    # ybins = _to_tuple
+    # zbins = _to_tuple
+
+class key:
+    def __init__(self, value):
+        self.value = value
 
 class _TsIniTransformerPhase1(Transformer):
-    
+
+    @staticmethod
+    def _dict_from_tuples(items):
+        names = set(item[0] for item in items)
+        if len(items)!=len(names): 
+            raise KeyError('')
+        return dict(((item[0], item[1]) for item in items))    
+
+    @staticmethod
+    def _dict_from_types(items):
+        def _dict_key(items):
+            return more_itertools.first_true(items, default=None, pred=lambda t: isinstance(t[1], key))
+
+        def _to_keyed_tuple(child_item):
+            if isinstance(child_item, dict):
+                key_item = more_itertools.first_true(child_item.items(), default=None, pred=lambda t: isinstance(t[1], key))
+                child_item[key_item[0]] = key_item[1].value
+                key_value = key_item[1].value                
+            elif isinstance(child_item, tuple) and isinstance(child_item[1], dict):
+                key_item = more_itertools.first_true(child_item[1].items(), default=None, pred=lambda t: isinstance(t[1], key))
+                child_item[1][key_item[0]] = key_item[1].value
+                key_value = key_item[1].value
+            elif isinstance(child_item, tuple):
+                index = next(i for i,v in enumerate(child_item[1]) if isinstance(v[1], key))
+                child_item[1][index] = (child_item[1][index][0], child_item[1][index][1].value)
+                key_value = child_item[1][index][1]
+            elif isinstance(child_item, list):
+                index = next(i for i,v in enumerate(child_item) if isinstance(v, key))
+                child_item[index] = child_item[index].value
+                key_value = child_item[index]
+
+            return (key_value, child_item)
+
+        return _TsIniTransformerPhase1._dict_from_tuples([_to_keyed_tuple(i) for i in items])
+
+    # Hoist all tokens into their parent node
     def __default_token__(self, token):
         if isinstance(token.value, str):
             return token.value.strip('"')
         return token.value
 
-    # Hoist these rules to just their child
-    def _hoist_children(self, children):
-        return children[0] if len(children)==1 else children
+    def transform_hoist_children(self, children):
+        return self.__class__._hoist_children_tag
+    def transform_tuple_only(self, children):
+        return self.__class__._tuple_only_tag 
+    def transform_dict_from_tuples(self, children):
+        return self.__class__._dict_from_tuple_tag        
+    def transform_dict_from_types(self, children):
+        return self.__class__._dict_from_types_tag
+    def transform_is_key(self, children):
+        return self.__class__._is_key_tag
+    def transform_hoist_only_child(self, children):
+        return self.__class__._hoist_child_tag
+        
+    _hoist_children_tag = transform_hoist_children.__name__
+    _tuple_only_tag = transform_tuple_only.__name__
+    _dict_from_tuple_tag = transform_dict_from_tuples.__name__
+    _dict_from_types_tag = transform_dict_from_types.__name__
+    _is_key_tag = transform_is_key.__name__
+    _hoist_child_tag = transform_hoist_only_child.__name__
 
-    char_constant = _hoist_children
-    floating_constant = _hoist_children
-    binary_constant = _hoist_children
-    decimal_int_constant = _hoist_children
-    hex_constant = _hoist_children
-    string_literal = _hoist_children
-    string_value = _hoist_children
-    number_field = _hoist_children
-    identifier = _hoist_children
-    help_line_key = _hoist_children
-    label = _hoist_children
-    help_line = _hoist_children
-    table_header = _hoist_children
+    _transform_tags = [
+        _hoist_children_tag,
+        _tuple_only_tag,
+        _hoist_child_tag,
+        _dict_from_tuple_tag,
+        _dict_from_types_tag,
+    ]
 
-    # Collapse these to tuples so we can use them in dictionary comprehensions
-    @v_args(tree=True)
-    def _to_tuple(self, tree):
-        return (tree.data, tree.children[0] if len(tree.children)==1 else tree.children)
-    kvp_key = _to_tuple
-    data_type = _to_tuple
-    offset = _to_tuple
-    units = _to_tuple
-    scale = _to_tuple
-    low = _to_tuple
-    high = _to_tuple
-    digits = _to_tuple
-    translate = _to_tuple
-    dim1d = _to_tuple
-    encoding = _to_tuple
-    length = _to_tuple
-    table_id = _to_tuple
-    map3d_id = _to_tuple
-    title = _to_tuple
-    page_num = _to_tuple
-    grid_height = _to_tuple
-    section_name = _to_tuple   
-    symbol = _to_tuple
-    defined_value = _to_tuple
-    grid_orient = _to_tuple
-    curve_id = _to_tuple
-    curve_name = _to_tuple
-    min = _to_tuple
-    max = _to_tuple
-    step = _to_tuple
-    constant_ref = _to_tuple
-    outputchannel = _to_tuple
-    name = _to_tuple
-    code_override = _to_tuple
-    help_topic = _to_tuple
-    name = _to_tuple
-    size = _to_tuple
-    gauge = _to_tuple
-    line_label = _to_tuple
-    bit_size = _to_tuple
-    dim2d = _to_tuple
+    # Apply defaults based empty rules (which act as tags)
+    def __default__(self, data, children, meta):       
+        transform_tags = [c for c in children if c in self.__class__._transform_tags]
+        children = [c for c in children if c not in transform_tags]
 
-    @v_args(tree=True)
-    def _to_list(self, tree):
-        return (tree.data, tree.children)
-    unknown_values = _to_list        
-    columnlabels = _to_list
-    xy_labels = _to_list
-    updown_labels = _to_list
+        if self.__class__._is_key_tag in children:
+            children = [c for c in children if c!=self.__class__._is_key_tag]
+            # Keys have to be singular
+            if len(children)>1:
+                raise ValueError()
+            children[0] = key(children[0])
+
+        # _dict_from_tuple_tag==dictionary from raw tuples (item[0]==key)
+        if self.__class__._dict_from_tuple_tag in transform_tags:
+            children = self.__class__._dict_from_tuples(children)
+
+        # _dict_from_types_tag==dictionary from typed tuples (item[0]==type, so key is in item[1]), no tuple
+        if self.__class__._dict_from_types_tag in transform_tags:
+            children = self.__class__._dict_from_types(children)
+
+        # _hoist_children_tag == Pass children up to parent
+        if self.__class__._hoist_children_tag in transform_tags:
+            return children
+
+        # _tuple_only_tag == convert to tuple, with ONLY ONE child
+        if self.__class__._tuple_only_tag in transform_tags:
+            if len(children)>1:
+                raise ValueError()
+            return (data, children[0] if children else None)
+
+        if self.__class__._hoist_child_tag in transform_tags:
+            if len(children)>1:
+                raise ValueError()
+            return children[0] if children else None
+
+        # Default - convert to a tuple, with the tree node type as item[0]
+        return (data, children)      
+    
