73377ad6965aedee84003a1e7c0ddaad77372618
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2021-08-26 20:26:39 -0500

    Separate out child processing rules & return rule

--- TsIniParser/DataClassTransformer.py
+++ TsIniParser/DataClassTransformer.py
@@ -14,41 +14,56 @@ class DataClassTransformer(Transformer):
         if len(items)!=len(names): 
             raise KeyError('')
         return dict(((item[0], item[1]) for item in items))    
+    
+    def _dict_from_types(self, data, children):
+        return self.__class__._dict_from_tuples([(i.key, i) for i in children])
 
-    @staticmethod
-    def _dict_from_types(items):
-        return DataClassTransformer._dict_from_tuples([(i.key, i) for i in items])
+    def _collapse_dups(self, data, children):
+        dup_map = {}
+        for key, value in children:
+            if key in dup_map:
+                if not isinstance(dup_map[key], list):
+                    dup_map[key] = [dup_map[key]]
+                dup_map[key].append(value)
+            else:
+                dup_map[key] = value
+        return [(key, value) for key, value in dup_map.items()]
+
+    def _to_type(self, data, children):
+        return self._factory(data, self.__class__._dict_from_tuples(children))
+
+    def _tuple_only(self, data, children):
+        if len(children)>1:
+            raise ValueError()
+        return (data, children[0] if children else None)  
 
     # Hoist all tokens into their parent node
     def __default_token__(self, token):
         if isinstance(token.value, str):
             return token.value.strip('"')
         return token.value
+       
+    _hoist_children_tag = 'transform_hoist_children'
+    _tuple_only_tag = 'transform_tuple_only'
+    _dict_from_types_tag = 'transform_dict_from_types'
+    _to_type_tag = 'transform_type_from_dict'
+    _collapse_dups_tag = 'transform_collapse_dups'
+    _to_tagged_tuple_tag = 'transform_to_tagged_tuple'
 
-    def transform_hoist_children(self, children):
-        return self.__class__._hoist_children_tag
-    def transform_tuple_only(self, children):
-        return self.__class__._tuple_only_tag 
-    def transform_dict_from_types(self, children):
-        return self.__class__._dict_from_types_tag
-    def transform_type_from_dict(self, children):
-        return self.__class__._to_type_tag
-    def transform_collapse_dups(self, children):
-        return self.__class__._collapse_dups_tag
-        
-    _hoist_children_tag = transform_hoist_children.__name__
-    _tuple_only_tag = transform_tuple_only.__name__
-    _dict_from_types_tag = transform_dict_from_types.__name__
-    _to_type_tag = transform_type_from_dict.__name__
-    _collapse_dups_tag = transform_collapse_dups.__name__
+    _child_processing_actions = {
+        _collapse_dups_tag:_collapse_dups,
+        _dict_from_types_tag:_dict_from_types,
+        _tuple_only_tag:_tuple_only,
+        _to_type_tag:_to_type,
+    }
+    _child_processing_tags = list(_child_processing_actions.keys())
 
-    _transform_tags = [
-        _dict_from_types_tag,
-        _collapse_dups_tag,
-        _to_type_tag,
-        _hoist_children_tag,
-        _tuple_only_tag,
-    ]
+    _return_actions = {
+        _hoist_children_tag: lambda data, children: children,
+        _to_tagged_tuple_tag : lambda data, children: (data, children)
+    }
+    _return_action_tags = list(_return_actions.keys())
+    _default_return_action = _return_actions[_to_tagged_tuple_tag]
 
     @staticmethod
     def _collapse_variable_refs(children): 
@@ -61,42 +76,25 @@ class DataClassTransformer(Transformer):
         return flat_list
 
     # Apply defaults based empty rules (which act as tags)
-    def __default__(self, data, children, meta):       
-        transform_tags = [c for c in children if c in self.__class__._transform_tags]
-        children = self.__class__._collapse_variable_refs((c for c in children if c not in transform_tags))
+    def __default__(self, data, children, meta):
+        # Base rule processing of root transform tag rule
+        if data=='transform_to_rulename' or 'transform_to_rulename' in children:
+            return data
 
-        if self.__class__._collapse_dups_tag in transform_tags:
-            dup_map = {}
-            for key, value in children:
-                if key in dup_map:
-                    if not isinstance(dup_map[key], list):
-                        dup_map[key] = [dup_map[key]]
-                    dup_map[key].append(value)
-                else:
-                    dup_map[key] = value
-            children = [(key, value) for key, value in dup_map.items()]
+        processing_action_tags = [c for c in children if c in self.__class__._child_processing_tags]
+        return_action = [c for c in children if c in self.__class__._return_action_tags]
+        children = self.__class__._collapse_variable_refs((c for c in children if c not in processing_action_tags and c not in return_action))
+        
+        for action_tag in processing_action_tags:
+            children = self.__class__._child_processing_actions[action_tag](self, data, children)
 
-        # _dict_from_types_tag==dictionary from typed tuples (item[0]==type, so key is in item[1]), no tuple
-        if self.__class__._dict_from_types_tag in transform_tags:
-            children = self.__class__._dict_from_types(children)
+        if len(return_action)>1:
+            raise ValueError()
+        return_action = self.__class__._return_actions[return_action[0]] if return_action else self.__class__._default_return_action
+        return return_action(data, children)
 
-        # _hoist_children_tag == Pass children up to parent
-        if self.__class__._hoist_children_tag in transform_tags:
-            return children
+    # ================== #define, $symbol handling =====================
 
-        # _tuple_only_tag == convert to tuple, with ONLY ONE child
-        if self.__class__._tuple_only_tag in transform_tags:
-            if len(children)>1:
-                raise ValueError()
-            return (data, children[0] if children else None)
-
-        # Convert to a dataclass based type
-        if self.__class__._to_type_tag in transform_tags:
-            return self._factory(data, self.__class__._dict_from_tuples(children))
-
-        # Default - convert to a tuple, with the tree node type as item[0]
-        return (data, children)      
-    
     def hashdef(self, children):
         self._symbols[children[0][1]] = self.__class__._collapse_variable_refs(children[1:])
         raise Discard
