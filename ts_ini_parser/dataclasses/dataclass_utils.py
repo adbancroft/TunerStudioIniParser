3f7d78e1aad95b4e7bf28fa2bc585cd5b10dd407
tx_haggis <13982343+adbancroft@users.noreply.github.com>
2021-09-03 13:01:59 -0500

    Fix lint issue

--- ts_ini_parser/dataclasses/dataclass_utils.py
+++ ts_ini_parser/dataclasses/dataclass_utils.py
@@ -4,6 +4,6 @@ from dataclasses import fields
 
 
 def from_existing(existing, new_type: Type, new_fields: Dict[str, Any]):
-    for f in fields(existing):
-        new_fields[f.name] = getattr(existing, f.name)
+    for field in fields(existing):
+        new_fields[field.name] = getattr(existing, field.name)
     return new_type(**new_fields)
